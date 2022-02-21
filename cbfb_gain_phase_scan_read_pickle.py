0a0f2584ccc569ba5ba597e8107729dec1baac0d
Jan Paszkiewicz <janpaszkiewicz92@gmail.com>
2022-02-21 15:18:06 +0100

    Misnamed variables.

--- cbfb_gain_phase_scan_read_pickle.py
+++ cbfb_gain_phase_scan_read_pickle.py
@@ -61,7 +61,7 @@ dipole_fb_phases = np.empty(N_runs_fb)
 dipole_exc_dipole_sideband_mag = np.empty(N_runs_fb)
 dipole_exc_quad_sideband_mag = np.empty(N_runs_fb)
 dipole_exc_pos_amp = np.empty((N_runs_fb, N_modes))
-dipole_exc_width_amp =  np.empty((N_runs_fb, N_modes))
+dipole_exc_width_amp = np.empty((N_runs_fb, N_modes))
 
 for run in range(N_runs_fb):
     run_dir = working_dir + scans_dir + 'dipole_run' + str(run+1) + '/'
@@ -74,7 +74,7 @@ for run in range(N_runs_fb):
     dipole_fb_phases[run] = np.angle(data['params'].cbfb_params['gain'][0][0])
     
     dipole_exc_pos_amp[run, :] = data['pos_mode_amp']
-    dipole_exc_quad_sideband_mag[run, :] = data['width_mode_amp']
+    dipole_exc_width_amp[run, :] = data['width_mode_amp']
 
 dipole_unique_gains = np.unique(dipole_fb_gains.round(decimals=8))
 
@@ -96,7 +96,7 @@ for run in range(N_runs_fb):
     quad_fb_gains[run] = np.abs(data['params'].cbfb_params['gain'][0][0])
     quad_fb_phases[run] = np.angle(data['params'].cbfb_params['gain'][0][0])
     quad_exc_pos_amp[run, :] = data['pos_mode_amp']
-    dipole_exc_quad_sideband_mag[run, :] = data['width_mode_amp']
+    quad_exc_width_amp[run, :] = data['width_mode_amp']
 
 quad_unique_gains = np.unique(quad_fb_gains.round(decimals=8))
 
@@ -124,8 +124,8 @@ for mode in plot_modes:
     for i in range(quad_unique_gains.shape[0]):
         plot_indices = np.isclose(quad_fb_gains, quad_unique_gains[i], rtol=1e-3)
         # plot_indices = (quad_fb_gains == quad_unique_gains[i])
-        plt.semilogy(quad_fb_phases[plot_indices], quad_exc_pos_amp[plot_indices, mode] /\
-                 base_quad_pos_mode_amp[mode], label='Gain = ' + str(quad_unique_gains[i]))
+        plt.semilogy(quad_fb_phases[plot_indices], quad_exc_width_amp[plot_indices, mode] /\
+                 base_quad_width_mode_amp[mode], label='Gain = ' + str(quad_unique_gains[i]))
     plt.legend(loc=0, fontsize='medium')
     plt.xlabel('CBFB phase [rad]')
     plt.ylabel('Relative oscillation amplitude')
