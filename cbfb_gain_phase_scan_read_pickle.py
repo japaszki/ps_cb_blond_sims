3897f75a4bd75468da2f29a7ebd09de379b9b1ea
Jan Paszkiewicz <janpaszkiewicz92@gmail.com>
2022-02-21 15:08:12 +0100

    Allowing for numerical noise in gain values.

--- cbfb_gain_phase_scan_read_pickle.py
+++ cbfb_gain_phase_scan_read_pickle.py
@@ -22,7 +22,7 @@ class sim_params:
     pass
 
 N_runs_fb = 160
-plot_mode = 20
+plot_modes = [1,20]
     
 working_dir = os.getcwd()
 scans_dir = '/scans/cbfb_baseline_gain_phase_scan/'
@@ -37,8 +37,9 @@ with open(run_dir + 'results.pickle', 'rb') as f:
 base_dipole_cbfb_usb = data['cbfb_usb_mag']
 base_dipole_cbfb_lsb = data['cbfb_lsb_mag']
 
-base_dipole_pos_mode_amp = data['pos_mode_amp'][plot_mode]
-base_dipole_width_mode_amp = data['width_mode_amp'][plot_mode]
+base_dipole_pos_mode_amp = data['pos_mode_amp']
+base_dipole_width_mode_amp = data['width_mode_amp']
+N_modes = base_dipole_pos_mode_amp.shape[0]
 
 #Baseline quad run:
 run_dir = working_dir + scans_dir + 'quad_run' + str(0) + '/'
@@ -49,8 +50,8 @@ with open(run_dir + 'results.pickle', 'rb') as f:
 base_quad_cbfb_usb = data['cbfb_usb_mag']
 base_quad_cbfb_lsb = data['cbfb_lsb_mag']
 
-base_quad_pos_mode_amp = data['pos_mode_amp'][plot_mode]
-base_quad_width_mode_amp = data['width_mode_amp'][plot_mode]
+base_quad_pos_mode_amp = data['pos_mode_amp']
+base_quad_width_mode_amp = data['width_mode_amp']
 
 #Paramter scans with feedback:
 
@@ -59,8 +60,8 @@ dipole_fb_gains = np.empty(N_runs_fb)
 dipole_fb_phases = np.empty(N_runs_fb)
 dipole_exc_dipole_sideband_mag = np.empty(N_runs_fb)
 dipole_exc_quad_sideband_mag = np.empty(N_runs_fb)
-dipole_exc_pos_amp = np.empty(N_runs_fb)
-dipole_exc_width_amp =  np.empty(N_runs_fb)
+dipole_exc_pos_amp = np.empty((N_runs_fb, N_modes))
+dipole_exc_width_amp =  np.empty((N_runs_fb, N_modes))
 
 for run in range(N_runs_fb):
     run_dir = working_dir + scans_dir + 'dipole_run' + str(run+1) + '/'
@@ -71,18 +72,19 @@ for run in range(N_runs_fb):
      
     dipole_fb_gains[run] = np.abs(data['params'].cbfb_params['gain'][0][0])
     dipole_fb_phases[run] = np.angle(data['params'].cbfb_params['gain'][0][0])
-    dipole_exc_pos_amp[run] = data['pos_mode_amp'][plot_mode]
-    dipole_exc_quad_sideband_mag[run] = data['width_mode_amp'][plot_mode]
+    
+    dipole_exc_pos_amp[run, :] = data['pos_mode_amp']
+    dipole_exc_quad_sideband_mag[run, :] = data['width_mode_amp']
 
-dipole_unique_gains = np.unique(dipole_fb_gains)
+dipole_unique_gains = np.unique(dipole_fb_gains.round(decimals=8))
 
 #Get data for quadrupole runs:
 quad_fb_gains = np.empty(N_runs_fb)
 quad_fb_phases = np.empty(N_runs_fb)
 quad_exc_dipole_sideband_mag = np.empty(N_runs_fb)
 quad_exc_quad_sideband_mag = np.empty(N_runs_fb)
-quad_exc_pos_amp = np.empty(N_runs_fb)
-quad_exc_width_amp =  np.empty(N_runs_fb)
+quad_exc_pos_amp = np.empty((N_runs_fb, N_modes))
+quad_exc_width_amp =  np.empty((N_runs_fb, N_modes))
 
 for run in range(N_runs_fb):
     run_dir = working_dir + scans_dir + 'quad_run' + str(run+1) + '/'
@@ -93,10 +95,10 @@ for run in range(N_runs_fb):
     
     quad_fb_gains[run] = np.abs(data['params'].cbfb_params['gain'][0][0])
     quad_fb_phases[run] = np.angle(data['params'].cbfb_params['gain'][0][0])
-    quad_exc_pos_amp[run] = data['pos_mode_amp'][plot_mode]
-    dipole_exc_quad_sideband_mag[run] = data['width_mode_amp'][plot_mode]
+    quad_exc_pos_amp[run, :] = data['pos_mode_amp']
+    dipole_exc_quad_sideband_mag[run, :] = data['width_mode_amp']
 
-quad_unique_gains = np.unique(quad_fb_gains)
+quad_unique_gains = np.unique(quad_fb_gains.round(decimals=8))
 
 try:
     os.makedirs(this_directory + 'cbfb_gain_phase_scan_plots/')
@@ -104,24 +106,28 @@ except:
     pass
 
 #Relative mode amplitude after CBFB on:
-plt.figure('dipole_amp_vs_phase')
-for i in range(dipole_unique_gains.shape[0]):
-    plot_indices = (dipole_fb_gains == dipole_unique_gains[i])
-    plt.semilogy(dipole_fb_phases[plot_indices], dipole_exc_pos_amp[plot_indices] /\
-             base_dipole_pos_mode_amp, label='Gain = ' + str(dipole_unique_gains[i]))
-plt.legend(loc=0, fontsize='medium')
-plt.xlabel('CBFB phase [rad]')
-plt.ylabel('Relative oscillation amplitude')
-plt.title('Dipole mode')
-plt.savefig(this_directory + 'cbfb_gain_phase_scan_plots/dipole_amp_vs_phase.png')
+for mode in plot_modes:
+    plt.figure('dipole_amp_vs_phase')
+    for i in range(dipole_unique_gains.shape[0]):
+        plot_indices = np.isclose(dipole_fb_gains, dipole_unique_gains[i], rtol=1e-3)
+        # plot_indices = (dipole_fb_gains == dipole_unique_gains[i])
+        plt.semilogy(dipole_fb_phases[plot_indices], dipole_exc_pos_amp[plot_indices, mode] /\
+                 base_dipole_pos_mode_amp[mode], label='Gain = ' + str(dipole_unique_gains[i]))
+    plt.legend(loc=0, fontsize='medium')
+    plt.xlabel('CBFB phase [rad]')
+    plt.ylabel('Relative oscillation amplitude')
+    plt.title('Dipole mode ' + str(mode))
+    plt.savefig(this_directory + 'cbfb_gain_phase_scan_plots/dipole_amp_vs_phase_mode_' + str(mode) + '.png')
 
-plt.figure('quad_amp_vs_phase')
-for i in range(quad_unique_gains.shape[0]):
-    plot_indices = (quad_fb_gains == quad_unique_gains[i])
-    plt.semilogy(quad_fb_phases[plot_indices], quad_exc_pos_amp[plot_indices] /\
-             base_quad_pos_mode_amp, label='Gain = ' + str(quad_unique_gains[i]))
-plt.legend(loc=0, fontsize='medium')
-plt.xlabel('CBFB phase [rad]')
-plt.ylabel('Relative oscillation amplitude')
-plt.title('Qudrupole mode')
-plt.savefig(this_directory + 'cbfb_gain_phase_scan_plots/quad_amp_vs_phase.png')
+for mode in plot_modes:
+    plt.figure('quad_amp_vs_phase')
+    for i in range(quad_unique_gains.shape[0]):
+        plot_indices = np.isclose(quad_fb_gains, quad_unique_gains[i], rtol=1e-3)
+        # plot_indices = (quad_fb_gains == quad_unique_gains[i])
+        plt.semilogy(quad_fb_phases[plot_indices], quad_exc_pos_amp[plot_indices, mode] /\
+                 base_quad_pos_mode_amp[mode], label='Gain = ' + str(quad_unique_gains[i]))
+    plt.legend(loc=0, fontsize='medium')
+    plt.xlabel('CBFB phase [rad]')
+    plt.ylabel('Relative oscillation amplitude')
+    plt.title('Qudrupole mode ' + str(mode))
+    plt.savefig(this_directory + 'cbfb_gain_phase_scan_plots/quad_amp_vs_phase_mode_' + str(mode) + '.png')
