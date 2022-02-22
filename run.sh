c69fb1b858b88e7a783253cf7ae357a23bbd41ff
Jan Paszkiewicz <janpaszkiewicz92@gmail.com>
2022-02-22 11:46:50 +0100

    Moving back to AFS since submissions involving EOS paths are currently not allowed.

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_96python3 x86_64-centos7-gcc8-opt
 
-export PYTHONPATH="/eos/user/j/japaszki/blond_sims/BLonD/:$PYTHONPATH"
-export PYTHONPATH="/eos/user/j/japaszki/blond_sims/ps_cb_blond_sims/:$PYTHONPATH"
+export PYTHONPATH="/afs/cern.ch/user/j/japaszki/blond_sims/blond/:$PYTHONPATH"
+export PYTHONPATH="/afs/cern.ch/user/j/japaszki/blond_sims/ps_cb_blond_sims/:$PYTHONPATH"
 
-python /eos/user/j/japaszki/blond_sims/ps_cb_blond_sims/run.py
+python /afs/cern.ch/user/j/japaszki/blond_sims/ps_cb_blond_sims/run.py
 
 tar -zcvf results.tar.gz plots.tar.gz sim_outputs
